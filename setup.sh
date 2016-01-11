3554d048dfe673a36b5ee7328c26c28920184fa1
Yifan Zhang <yifan.zhang@uwaterloo.ca>
2016-01-11 11:20:20 -0500

    Attempt to centralize all dotfiles

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
