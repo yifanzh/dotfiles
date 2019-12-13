d30f50e8e9a4d7b15eebd2b7f518ab493277638d
Yifan Zhang <yifan.zhang@akunacapital.com>
2019-12-13 15:33:39 -0600

    more shell tweaks: use two line

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,6 +6,6 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status_color (set_color -o red)
     end
 
-    printf '%s[%s] %s@%s %s \f\r%s>%s ' (set_color -o blue) (date "+%H:%M:%S") $USER (prompt_hostname) (prompt_pwd) "$prompt_status_color" "$__fish_prompt_normal"
+    printf '%s[%s] %s@%s %s \f\r%s>%s ' (set_color -o blue) (date "+%H:%M:%S") $USER (prompt_hostname) "$PWD" "$prompt_status_color" "$__fish_prompt_normal"
 
 end
