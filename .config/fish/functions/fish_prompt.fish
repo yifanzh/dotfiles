9b1478c9ac3c6d70afeb10901853fcb83153e549
Yifan Zhang <yifanzhang@hotmail.ca>
2020-11-30 09:59:10 -0500

    update fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,25 @@
-function fish_prompt
-	echo -n '$ '
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
+
+    # User
+    set_color normal
+    echo -n $USER
+
+    echo -n '@'
+
+    # Host
+    set_color $fish_color_host
+    echo -n (prompt_hostname)
+    set_color normal
+
+    echo -n ' '
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    else
+        set_color $fish_color_end
+    end
+
+    echo -n '$ '
+    set_color normal
 end
